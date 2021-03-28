91b9d004686882a777616f74a7c0fe54376dc40e
yuikim <nokdoo00@gmail.com>
2021-03-28 16:12:20 -0400

    add icons and core components of material-ui, set types, and fetch fakestoreapi

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+
+import { Wrapper } from './App.styles';
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
 const App = () => {
   return <div className='App'>Start</div>;
 };
