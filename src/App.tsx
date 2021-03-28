513908960b159ce829ff7c107e3c1f4069965372
yuikim <nokdoo00@gmail.com>
2021-03-28 16:55:35 -0400

    modify styles in Item.style.ts and Item componet

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 
+import Item from './Item/Item';
+
 import { Wrapper } from './App.styles';
 
 export type CartItemType = {
@@ -19,11 +21,37 @@ export type CartItemType = {
   amount: number;
 };
 
-const getProducts = async (): Promise<CartItemType> =>
+const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
-  return <div className='App'>Start</div>;
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products',
+    getProducts
+  );
+
+  console.log(data);
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveToCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Someting went wrong...</div>;
+
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map((item) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
 };
 
 export default App;
